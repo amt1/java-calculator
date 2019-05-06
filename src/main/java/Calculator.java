3ded7ebd76244706d8b767ef92779bfcebe0ecf9
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-05-06 14:43:43 +0100

    calculator functions done

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,4 +7,16 @@ public class Calculator {
     public int add(int int1, int int2){
         return int1 + int2;
     }
+
+    public int subtract(int int1, int int2){
+        return int1 - int2;
+    }
+
+    public int multiply(int int1, int int2){
+        return int1 * int2;
+    }
+
+    public float divide(float num1, float num2){
+        return  (num1 / num2);
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -16,4 +16,28 @@ public class CalculatorTest {
     public void canAdd(){
         assertEquals(5, calculator.add(2,3));
     }
+
+    @Test
+    public void canSubtract(){
+        assertEquals(5, calculator.subtract(8,3));
+    }
+
+    @Test
+    public void canSubtractNegative(){
+        assertEquals(-5, calculator.subtract(3,8));
+    }
+    @Test
+    public void canMultiply(){
+        assertEquals(24, calculator.multiply(8,3));
+    }
+
+    @Test
+    public void canDivide(){
+        assertEquals(8, calculator.divide(25,3), 0.01);
+    }
+
+    @Test
+    public void canDivideFloat(){
+        assertEquals(8.333333015441895, calculator.divide(25, 3), 0.01);
+    }
 }
